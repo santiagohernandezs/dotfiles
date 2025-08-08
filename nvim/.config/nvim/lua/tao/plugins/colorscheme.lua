9f9a7a5f4bce64c5b42afd67dff52a59131bc15a
Santiago Hernandez <santiagooheernandez@gmail.com>
2025-08-08 10:34:02 -0400

    up̀date nvim colorscheme

--- nvim/.config/nvim/lua/tao/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/tao/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
-	"stevedylandev/darkmatter-nvim",
+	"cdmill/neomodern.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme darkmatter")
+		require("neomodern").setup({
+			-- optional configuration here
+			theme = "roseprime",
+		})
+		require("neomodern").load()
 	end,
 }
